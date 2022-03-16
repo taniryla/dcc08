bd2d9dd07391c561689f6b3d3533033ad5616b44
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 09:48:49 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 08-removeEnds
+Difficulty: Basic
+Prompt:
+- Write a function called removeEnds that accepts a single string argument, then returns the a string with the first and last characters removed.
+- If the length of the string argument is less than 3, return an empty string.
+Examples:
+removeEnds('SEI Rocks!'); //=> "EI Rocks"
+removeEnds('a'); //=> "" (empty string)
+-----------------------------------------------------------------*/
+// Your solution for 08-removeEnds here:
+
+</div>
+`;
